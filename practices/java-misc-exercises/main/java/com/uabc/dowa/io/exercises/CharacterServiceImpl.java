59c3c6777bba753d55f095c1c2ed5e001623b1f4
Jesus Andres Morales Ramos <andresmor93@msn.com>
2015-03-03 00:41:53 -0800

    Terminada las funcionalidades para encontrar uno y dos numeros.

---
+++ practices/java-misc-exercises/main/java/com/uabc/dowa/io/exercises/CharacterServiceImpl.java
@@ -0,0 +1,21 @@
+package com.uabc.dowa.io.exercises;
+
+/**
+ * Created by Andres on 3/1/2015.
+ */
+public class CharacterServiceImpl implements CharacterService{
+    @Override
+    public boolean areAnagrams(String s1, String s2) {
+        return false;
+    }
+
+    @Override
+    public void reverse(char[] arr) {
+
+    }
+
+    @Override
+    public void reverseWords(char[] arr) {
+
+    }
+}
---
+++ practices/java-misc-exercises/test/java/com/uabc/dowa/io/exercises/CharacterServiceTest.java
@@ -0,0 +1,21 @@
+package com.uabc.dowa.io.exercises;
+
+import org.junit.Test;
+
+public class CharacterServiceTest {
+
+    @Test
+    public void areAnagrams() {
+
+    }
+
+    @Test
+    public void testReverse() {
+
+    }
+
+    @Test
+    public void testReverseWords() {
+
+    }
+}
