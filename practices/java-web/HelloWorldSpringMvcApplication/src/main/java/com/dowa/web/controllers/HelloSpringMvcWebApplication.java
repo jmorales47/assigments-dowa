8a5d55d2f8d6db6321446cebcb8b9c3e9d0c7942
J Andres <jmorales47@github.com>
2015-05-01 22:45:07 -0700

    Terminada primera practica de Spring

---
+++ practices/java-web/HelloWorldSpringMvcApplication/src/main/java/com/dowa/web/controllers/HelloSpringMvcWebApplication.java
@@ -0,0 +1,22 @@
+package com.dowa.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloSpringMvcWebApplication {
+    @RequestMapping(value = "/hello-spring-mvc/", method = RequestMethod.GET)
+    public String helloWorld(Model model) {
+        model.addAttribute("message", "Star Wars Rocks!");
+        return "index";
+    }
+    @RequestMapping(value = "/hello-spring-mvc-params", method = RequestMethod.POST)
+    public String welcome(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "welcome";
+    }
+}
+
